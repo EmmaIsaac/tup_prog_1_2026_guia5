2984ff40d55aefb452bde006893e2240c7d3b150
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2026-05-06 15:14:22 -0300

    Resolucion ejercicio3

---
+++ Guia5/Ejercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,acu;
+            double prom;
+
+            acu = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese numero:");
+                n=Convert.ToInt32(Console.ReadLine());
+                acu = acu + n;
+            }
+            prom = acu / 10.0;
+            Console.WriteLine("El promedio es: " + prom);
+            Console.ReadKey();
+        }
+    }
+}
