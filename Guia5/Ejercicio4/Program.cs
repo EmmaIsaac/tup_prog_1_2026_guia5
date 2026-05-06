94ad409a1783e8d4149c08a0b2a6dafa98a0fe12
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2026-05-06 15:53:03 -0300

    Resolucion Ejercicio4

---
+++ Guia5/Ejercicio4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1,n2,dividendo;
+
+            Console.WriteLine("Ingrese primer numero:");
+            n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 < n2)
+            {
+                for (int i = n1; i < n2; i++)
+                {
+                    dividendo = i + 1;
+                    if (dividendo % 2 == 0)
+                    {
+                        Console.WriteLine($"{dividendo} es divisible por 2");
+                    }
+                    if (dividendo % 3 == 0)
+                    {
+                        Console.WriteLine($"{dividendo} es divisible por 3");
+                    }
+                }
+            }
+            else if (n2 < n1)
+            {
+                for (int i = n2; i < n1; i++)
+                {
+                    dividendo = i + 1;
+                    if (dividendo % 2 == 0)
+                    {
+                        Console.WriteLine($"{dividendo} es divisible por 2");
+                    }
+                    if (dividendo % 3 == 0)
+                    {
+                        Console.WriteLine($"{dividendo} es divisible por 3");
+                    }
+                }
+            }
+            else {
+                Console.WriteLine("Los numeros ingresados son iguales");
+            }            
+            Console.ReadKey();
+        }
+    }
+}
